1e0371c478ed9b34fafeedb6ebff366c02b7ef87
ASUS <waghmodeajinkya80@gmail.com>
2024-02-11 23:35:35 +0530

    1st commit

---
+++ app/src/main/java/com/example/quadraticeqationdatabinding/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.quadraticeqationdatabinding;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
+
+import android.os.Bundle;
+
+import com.example.quadraticeqationdatabinding.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+
+    ActivityMainBinding activityMainBinding;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        activityMainBinding= DataBindingUtil.setContentView(this, R.layout.activity_main);
+
+        MyEquation myEquation=new MyEquation(activityMainBinding);
+
+        activityMainBinding.setMyequation(myEquation);
+
+    }
+}
